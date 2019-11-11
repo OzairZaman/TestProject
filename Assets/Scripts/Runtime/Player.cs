2235417b81f3167ee5bdfa19a00d4b6507daf7f8
Ozair Zaman <48018195+OzairZaman@users.noreply.github.com>
2019-11-11 14:10:20 +1100

    Cleaned Code and added tests

---
+++ Assets/Scripts/Runtime/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+    private Rigidbody rigid;
+    private Collider col;
+
+    #region Internal
+    void Reset()
+    {
+        col = GetComponent<Collider>();
+        rigid = GetComponent<Rigidbody>();
+        rigid.useGravity = true;
+
+        col.material = Resources.Load<PhysicMaterial>("Physics/Slide");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        Move(new Vector3(inputH, 0f, inputV), movementSpeed);
+    }
+    #endregion
+
+    #region External
+    public void Move(Vector3 direction, float force)
+    {
+        rigid.AddForce(direction * force);
+    }
+    #endregion
+}
--- Assets/Tests/SampleTests.cs
+++ Assets/Tests/SampleTests.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework;
+
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.Assertions;
 
 namespace Tests
 {
     public class SampleTests
     {
+        public GameObject game;
+        public Player player;
+
+        // Sets up the Test Objects (Per Test)
+        [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            GameObject gamePrefab = Resources.Load<GameObject>("Prefabs/Game");
+            game = Object.Instantiate(gamePrefab);
+            player = game.GetComponentInChildren<Player>();
+            yield return null;
+        }
+
+        // Destroys the Test Objects (Per Test)
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
+            Object.Destroy(game);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerMovement()
+        {
+            // Get Original Position
+            Vector3 oldPosition = player.transform.position;
+
+            // A. Call Player's 'Move' function
+            player.Move(Vector3.right, 10);
+
+            // B. Wait until the end of the frame
+            yield return new WaitForFixedUpdate();
+
+            // Get New Position
+            Vector3 newPosition = player.transform.position;
+
+            // C. Player's Position should now be different
+            Assert.AreNotEqual(oldPosition, newPosition);
+        }
+
+
+
+
+
+
+
+
         // A Test behaves as an ordinary method
-        [Test]
-        public void SampleTestsSimplePasses()
+        [UnityTest]
+        public IEnumerator SampleTestsSimplePasses()
         {
             // Use the Assert class to test conditions
+            yield return null;
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
